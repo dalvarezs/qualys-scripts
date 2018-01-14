716e91dea1c7f563f432d1909974cd561815f046
dalvarezs <dalvarezs@users.noreply.github.com>
2018-01-14 12:44:24 +0100

    improvements and some error handling
    write results to a csv file

--- GetMapReports/qualys_mapreports.py
+++ GetMapReports/qualys_mapreports.py
@@ -4,23 +4,31 @@
 17/12/2017
 """
 
+import sys
 import contextlib
 import OpenSSL.crypto
 import requests
 from requests.auth import HTTPBasicAuth
 import tempfile
 import xml.etree.ElementTree as ET
+import getpass
+import time
+import socket #socket excpetion
 
 __author__ = 'David Alvarez @dalvarez_s'
-__version__ = '0.1'
+__version__ = '0.2'
 __doc__ = 'Connects to Qualys API. Get list of Map reports finished and extract hosts without DNSname ' \
           'UseCase: Useful for identifying live hosts without DNSname'
 
+#ToDo: Parallel HTTP requests
 
-QUSER = 'user'
-QPASSWD = 'password'
-QCERT_PASSWD = 'certpassword'
 
+QUSER = None
+QPASSWD = None
+QCERT_PASSWD = None
+#QUSER = 'user'
+#QPASSWD = 'password'
+#QCERT_PASSWD = 'certpassword'
 
 @contextlib.contextmanager
 def pfx_to_pem(pfx_path, pfx_password):
@@ -39,18 +47,42 @@ def pfx_to_pem(pfx_path, pfx_password):
         yield t_pem.name
 
 
-# Map Scan details
+# Extract Map Scan details
 def mapscanreport(datafeed):
     datafeed = bytes(bytearray(datafeed, encoding='utf-8'))
     root = ET.fromstring(datafeed)
 
     for ip in root.iter('IP'):
         if not (ip.get('name')):  # findall(".//MAP/IP[not(@name)]/@value") not supported by ElementTree's XPath version
-            print ip.get("value") + "," + str(ip.get("os"))
+            print ip.get("value") + ";" + str(ip.get("os"))
+            with open("mapscan.csv", "a") as f:
+                f.write(str(ip.get("value")) + ";" + str(ip.get("os")) + "\n")
+
+# Get a Map Scan Report
+def getmapscan(url, username, password, certpassword):
+    try: 
+        with pfx_to_pem('certificate.pem', certpassword) as qcert:
+            r = requests.get(url, auth=HTTPBasicAuth(username, password), headers={'X-Requested-With': 'testing'},cert=qcert)
+        mapscanreport(r.text)
+    except requests.ConnectionError as e:
+        print "Connection Error\n"
+        print str(e)
+    except requests.Timeout as e:
+        print "Timeout Error"
+        print str(e)
+    except requests.RequestException as e:
+        print "General Error"
+        print str(e)
+        #sys.exit(1)
+    except socket.error as e:
+        print "Socket Error"
+        print str(e)
+        getmapscan(url, username, password, certpassword) # Workaround to get a map report if socket exception was raised, but it could lead to an infinite loop
+        #sys.exit(1)
 
 
 # Map Scan Reports list
-def mapscanlist(datafeed):
+def mapscanlist(datafeed, username, password, certpassword):
     datafeed = bytes(bytearray(datafeed, encoding='utf-8'))
     root = ET.fromstring(datafeed)
 
@@ -58,14 +90,26 @@ def mapscanlist(datafeed):
             ".//MAP_REPORT[@status='FINISHED']"):  # findall(".//MAP_REPORT[@status='FINISHED']/@ref") not supported by ElementTree's XPath version
         print report.get("ref")
         url = 'https://certs.qualys.eu/msp/map_report.php?ref=' + str(report.get("ref"))
-        r = requests.get(url, auth=HTTPBasicAuth(QUSER, QPASSWD), headers={'X-Requested-With': 'testing'},
-                         cert=cert)
-        mapscanreport(r.text)
+        getmapscan(url, username, password, certpassword)
+        # Avoid Qualys API Limits
+        #time.sleep(1)
 
+def main():
+    if not QUSER or not QPASSWD or not QCERT_PASSWD:
+        try:
+            username = raw_input("Qualys Username: ")
+            password = getpass.getpass("Qualys Password: ")
+            certpassword = getpass.getpass("Qualys Certificate Password: ")
+        except KeyboardInterrupt, e:
+            sys.exit()
+    else:
+        username, password, certpassword = QUSER, QPASSWD, QCERT_PASSWD
+    
+    with pfx_to_pem('certificate.pem', certpassword) as qcert:
+        r = requests.get('https://certs.qualys.eu/msp/map_report_list.php', auth=HTTPBasicAuth(username, password), headers={'X-Requested-With': 'testing'}, cert=qcert)
+        # print r.text
 
-with pfx_to_pem('foo.pem', QCERT_PASSWD) as cert:
-    r = requests.get('https://certs.qualys.eu/msp/map_report_list.php', auth=HTTPBasicAuth(QUSER, QPASSWD),
-                     headers={'X-Requested-With': 'testing'}, cert=cert)
-    # print r.text
+        mapscanlist(r.text, username, password, certpassword)
 
-    mapscanlist(r.text)
+if __name__ == '__main__':
+    sys.exit(main())
